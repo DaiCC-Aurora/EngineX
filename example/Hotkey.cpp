ef651885e503116af5198bd3f2646453029c7122
DaiCC-Aurora <daicc.aurora@gmail.com>
2023-08-03 15:53:10 +0800

    useful 2

--- example/Hotkey.cpp
+++ example/Hotkey.cpp
@@ -4,23 +4,23 @@
 
 using namespace std;
 
-// 全局标志，用于控制循环打印"Helloworld"的线程是否运行
-bool isPrinting = true;
-
 // 函数原型
 void HotkeyThread();
-void PrintThread();
 
 int main() {
     // 创建热键检测线程
     thread hotkeyThread(HotkeyThread);
 
-    // 创建循环打印线程
-    thread printThread(PrintThread);
+    // 主线程循环打印"Helloworld"
+    while (true) {
+        cout << "Helloworld" << endl;
+        this_thread::sleep_for(chrono::seconds(1)); // 休眠1秒
 
-    // 主线程等待热键线程和打印线程执行完毕
+        // 可以在此处添加其他主线程需要处理的任务
+    }
+
+    // 等待热键线程执行完毕
     hotkeyThread.join();
-    printThread.join();
 
     return 0;
 }
@@ -35,19 +35,16 @@ void HotkeyThread() {
     MSG msg = { 0 };
     while (GetMessage(&msg, NULL, 0, 0) != 0) {
         if (msg.message == WM_HOTKEY) {
-            cout << "htk！" << endl;
+            cout << "热键被触发！" << endl;
 
-            // 设置标志为false，终止打印线程
+            // 可以在此处添加热键触发后需要处理的任务
+            // ...
+
+            // 热键触发后退出主循环，结束程序
+            break;
         }
     }
 
     // 注销热键
     UnregisterHotKey(NULL, 1);
-}
-
-void PrintThread() {
-    while (isPrinting) {
-        cout << "Helloworld" << endl;
-        this_thread::sleep_for(chrono::seconds(1)); // 休眠1秒
-    }
 }
